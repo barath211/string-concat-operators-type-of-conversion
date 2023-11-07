b5170b3b10c04df84ea3c97e4461ac7ff0dc35b4
barath <barath2112000@gmail.com>
2023-11-07 19:56:48 +0530

    string concataion

---
+++ operators.js
@@ -0,0 +1,16 @@
+// string concat
+
+// let firstName = 'virat';
+// let lastName = 'kholi';
+// let fullName = firstName +' ' + lastName ;
+// console.log(fullName)
+
+
+let sayHi = 'Hi'
+let mySelf = 'Barath'
+let city = 'coimbatore'
+let college = 'Bishop Ambrose'
+let year = 2021
+
+let intro = sayHi +' ' + 'im' + ' ' + mySelf + ' ' + 'im from' + ' ' + city  +  ' ' +  ' i complete my UG at ' + ' ' + college + ' passout in ' + ' ' + year
+console.log(intro);
